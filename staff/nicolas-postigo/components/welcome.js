3236e2776def20fec7feba96618697a1c509015f
Nicolas <nicolaspostigoausere@hotmail.es>
2020-11-02 15:13:46 +0100

    welcomes changes

--- staff/nicolas-postigo/components/welcome.js
+++ staff/nicolas-postigo/components/welcome.js
@@ -1,10 +1,10 @@
 function mountWelcome() {
-    Component.call(this, `<section>
+    var container = mountContainer(`<section>
     <h2>WELCOME</h2>
 
 <iframe width="350" height="300" src="https://www.youtube.com/embed/78jLBNSqc3g?rel=0&amp;autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </section>`)
+
+    return container
 }
 
-Welcome.prototype = Object.create(Component.prototype)
-Welcome.prototype.constructor = Welcome
