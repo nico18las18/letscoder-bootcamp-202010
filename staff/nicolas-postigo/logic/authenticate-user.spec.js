7f8b65b00426925e86604e0e3a6fcd77d4e17af7
Nicolas <nicolaspostigoausere@hotmail.es>
2020-11-02 20:46:03 +0100

    last changes of jasmine w/ henry

--- staff/nicolas-postigo/logic/authenticate-user.spec.js
+++ staff/nicolas-postigo/logic/authenticate-user.spec.js
@@ -14,19 +14,54 @@ describe('SPEC authenticateUser()', function () {
                     expect(status).toBe(201)
                     expect(response.length).toBe(0)
 
-                    done()
-                }
-            )
+                    call('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users/auth',
+                        { 'Content-type': 'application/json' },
+                        JSON.stringify({ username: email, password }),
+                        function (status, response) {
+                            expect(status).toBe(200)
+                            expect(response.length).toBeGreaterThan(0)
+
+                            token = JSON.parse(response).token
+
+                            expect(token.length).toBeGreaterThan(0)
+                            done()
+                        }
+                    )
+
+                    it('should succeed on authanticating an existing user', function (done) {
+                        authenticateUser(email, password, function (error, token) {
+
+                            expect(status).toBe(200)
+                            expect(response.length).toBeGreaterThan(0)
+
+                            token = JSON.parse(response).token
+
+                            expect(token.length).toBeGreaterThan(0)
+
+                            done()
+
+
+                        })
+                    })
+                    afterEach(function (done) {
+                        call('DELETE', 'https://b00tc4mp.herokuapp.com/api/v2/users',
+                            {
+                                Authorization: `Bearer ${token}`,
+                                'Content-type': 'application/json'
+                            },
+                            JSON.stringify({ password }),
+                            function (status, response) {
+                                expect(status).toBe(204)
+                                expect(response.length).toBe(0)
+
+                                done()
+                            })
+                    })
+
+                })
         })
-        it('should succeed on authanticating an existing user', function (done) {
-            authenticateUser(email, password, function (error) {
-                expect(error).toBeNull()
 
 
-                done()
 
-            })
-        })
-        
     })
 })
