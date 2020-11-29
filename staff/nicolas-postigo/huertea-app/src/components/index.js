1b7eecf47397c2890435f48ac706b1dff419f6b4
Nicolas <nicolaspostigoausere@hotmail.es>
2020-11-29 10:53:36 +0100

    all components defined

--- staff/nicolas-postigo/huertea-app/src/components/index.js
+++ staff/nicolas-postigo/huertea-app/src/components/index.js
@@ -3,11 +3,13 @@ import Login from './Login'
 import Home from './Home'
 import Hub from './Hub'
 import Createoffer from './Createoffer'
+import Detailoffer from './Detailoffer'
 
 export {
     Register,
     Login,
     Home,
     Hub,
-    Createoffer
+    Createoffer,
+    Detailoffer
 }
